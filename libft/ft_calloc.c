a5d4518fa7300664f29028ca309dcb39e350213b
lulm <lionelulm67@gmail.com>
2023-11-09 03:07:04 +0100

    Makefile done

---
+++ libft/ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lionelulm <lionelulm@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 02:10:40 by lionelulm         #+#    #+#             */
+/*   Updated: 2023/11/09 02:22:13 by lionelulm        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*p;
+
+	p = malloc(count * size);
+	if (!p)
+	{
+		return(NULL);
+	}
+	ft_bzero(p, (count * size));
+	return (p);
+}
