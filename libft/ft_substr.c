9385867967e625c842acb91b9c52b19e962b81d1
lulm <lionelulm67@gmail.com>
2023-11-15 05:51:30 +0100

    added ft_substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -1,33 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lionelulm <lionelulm@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/14 14:02:55 by lionelulm         #+#    #+#             */
-/*   Updated: 2023/11/15 04:20:05 by lionelulm        ###   ########.fr       */
+/*   Created: 2023/11/15 04:22:56 by lionelulm         #+#    #+#             */
+/*   Updated: 2023/11/15 05:51:09 by lionelulm        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*str;
-	int		i;
+	size_t	i;
+	char	*end;
 
-	i = 0;
-	str = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
-	if (str == 0);
-	{
+	end = (char *)malloc((sizeof(char) * i) + 1);
+	if (!s)
+		return (NULL);
+	if (len == 0 || ft_strlen(s) == 0 || start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (end == 0)
 		return (0);
-	}
-	while (s1[i] != '\0')
+	i = 0;
+	while (i < len && s[i + start])
 	{
-		str[i] = s1[i];
+		end[i] = s[i + start];
 		i++;
 	}
-	str[i] = '\0';
-	return (str);
+	end[i] = '\0';
+	return (end);
 }
+
