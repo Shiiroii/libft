f68e3a1a7c13c4e174a8869d099352970d59b89a
lulm <lionelulm67@gmail.com>
2023-11-17 05:47:23 +0100

    oui

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lulm <lulm@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: lionelulm <lionelulm@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 13:14:16 by lulm              #+#    #+#             */
-/*   Updated: 2023/11/16 13:26:08 by lulm             ###   ########.fr       */
+/*   Updated: 2023/11/17 05:47:16 by lionelulm        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 char	**ft_split(char const *s, char c);
 {
+	char	**tester;
+	size_t	i;
+	size_t	len;
 
+	tester = malloc(sizeof(char *) * (count(s, c) + 1));
+	i = 0;
+	if (!s || !c || !cara)
+	{
+		return (0);
+	}
+	while (*s != '\0')
+	{
+		if (*s != c)
+		{
+			len = 0;
+			while (*s )
+		}
+	}
 }
--- main/main_split.c
+++ main/main_split.c
@@ -1,24 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_strtrim.c                                     :+:      :+:    :+:   */
+/*   main_split.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lionelulm <lionelulm@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/17 04:13:18 by lionelulm         #+#    #+#             */
-/*   Updated: 2023/11/17 05:47:16 by lionelulm        ###   ########.fr       */
+/*   Created: 2023/11/17 05:08:16 by lionelulm         #+#    #+#             */
+/*   Updated: 2023/11/17 05:09:13 by lionelulm        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 int main()
 {
-		char	strs[10] = "Salut";
-		char	sets[10] = "S";
-		char	stre[10] = "Salut";
-		char	sete[10] = "t";
-	char	*start = ft_strtrim(strs, sets);
-	char	*end = ft_strtrim(stre, sete);
-	printf("result start: %s\n", start);
-	printf("result end: %s\n", end);
-	return 0;
+	char str[20] = "Oui bonjour c'est moi";
+	char **result = ft_strsplit(str, ' ');
+	printf("result:%s\n", *result);
+	printf("result:%s\n", result[0]);
+	printf("result:%s\n", result[1]);
+	return (0);
 }
