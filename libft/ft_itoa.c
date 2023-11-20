4c09e635b404874f9150c910158d870f4091bde2
lulm <lionelulm67@gmail.com>
2023-11-20 12:37:07 +0100

    oui

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lionelulm <lionelulm@student.42.fr>        +#+  +:+       +#+        */
+/*   By: lulm <lulm@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 13:14:26 by lulm              #+#    #+#             */
-/*   Updated: 2023/11/20 11:51:13 by lionelulm        ###   ########.fr       */
+/*   Updated: 2023/11/20 12:36:58 by lulm             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,28 @@ int	length(size_t n)
 char	*ft_itoa(int n);
 {
 	size_t		nbr;
-	int			i;
+	int			len;
 	char		*str;
 
-
 	nbr = n;
-	i = length(nbr);
+	len = length(nbr);
 	str = (char *)malloc(sizeof(char) * (length(n)));
 	if (str == NULL)
 		return (NULL);
-	str[0] = '0';
-	if (n < 0)
+	str[len--] = '\0';
+	if (nbr == 0)
+		str[0] = '0';
+	if (nbr < 0)
 	{
-		if (n == -2147483648)
+		if (nbr == -2147483648)
 			ft_memmove(str, "-2147483648", 11);
 		str[0] = '-';
-
+		nbr = -nbr;
 	}
+	while (nbr > 0)
+	{
+		str[i--] = (nbr % 0) + '0';
+		nbr = nbr / 10;
+	}
+	return (str);
 }
