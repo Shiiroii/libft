bbdac0b438fa789f1e779be33e52cc90e5954ef6
Shiroii || Lio <139677855+Shiiroii@users.noreply.github.com>
2023-12-11 15:16:54 +0100

    Update ft_strlcpy.c

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -18,9 +18,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 
 	i = 0;
 	if (size == 0)
-	{
 		return (ft_strlen(src));
-	}
 	while (src[i] != '\0' && i < size - 1)
 	{
 		dst[i] = src[i];
