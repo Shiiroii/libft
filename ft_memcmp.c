eb00fbf4534e9ef6a127442abbffeedc90f15a3c
lulm <lionelulm67@gmail.com>
2024-10-17 17:59:49 +0200

    added memchr and memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,32 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lionelulm <lionelulm@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/17 16:02:51 by liulm             #+#    #+#             */
-/*   Updated: 2024/10/17 17:58:30 by lionelulm        ###   ########.fr       */
+/*   Created: 2024/10/17 17:39:49 by lionelulm         #+#    #+#             */
+/*   Updated: 2024/10/17 17:59:10 by lionelulm        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*temp_str;
-	unsigned char	chr;
-	size_t			len;
 
-	temp_str = (unsigned char *)s;
-	chr = (unsigned char)c;
-	len = 0;
-	while(len < n)
-	{
-		if (temp_str[len] == chr)
-			return (temp_str + len);
-		len++
-	}
-	return (NULL);
 }
-Blah
+zv rarfa
